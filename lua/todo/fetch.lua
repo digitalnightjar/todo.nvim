9fabcfbfa12a824626a1a28d076ad12a9b9473e2
karl <digitalnightjar@outlook.com>
2024-05-21 23:48:20 +0100

    .

--- lua/todo/fetch.lua
+++ lua/todo/fetch.lua
@@ -1,7 +1,7 @@
 local oldreq = require
 local require = function(s) return oldreq('todo.deps' .. s) end
 -- Imports the module for handling SQLite.
-local sqlite = require("todo.deps.ljsqlite3")
+local sqlite = require("ljsqlite3")
 
 --- Restore old requires
 require = oldreq
