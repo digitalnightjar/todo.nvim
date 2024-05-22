efcdcc26b90da902d32a2960ec7687f5cf9b9f66
karl <digitalnightjar@outlook.com>
2024-05-22 01:58:49 +0100

    create plenary popup?

--- lua/todo/fetch.lua
+++ lua/todo/fetch.lua
@@ -1,13 +1,35 @@
---local oldreq = require
---:tequire = function(s) return oldreq('todo.deps.' .. s) end
--- Imports the module for handling SQLite.
 local sqlite = require("ljsqlite3")
-
---- Restore old requires
---require = oldreq
+local popup = require("plenary.popup")
 
 -- Creates an object for the module.
 local M = {}
+
+local function create_todolist_window()
+	local width = 100
+	local height = 100
+	local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+	local bufnr = vim.api.nvim_create_buf(false, false)
+	local todo_win_id, win = popup.create(bufnr, {
+        title = "Todos",
+        highlight = "TodosWindow",
+        line = math.floor(((vim.o.lines - height) / 2) - 1),
+        col = math.floor((vim.o.columns - width) / 2),
+        minwidth = width,
+        minheight = height,
+        borderchars = borderchars,
+    })
+
+    vim.api.nvim_win_set_option(
+        win.border.win_id,
+        "winhl",
+        "Normal:TodosBorder"
+    )
+
+    return {
+        bufnr = bufnr,
+        win_id = todo_win_id,
+    }
+end
 
 -- Fetches todo tasks from the database and
 -- prints the output.
@@ -16,8 +38,9 @@ function M.fetch_todos()
 
     local db_results = db:exec("SELECT * FROM todo_list WHERE completed == 'No';")
     for _, item in ipairs(db_results[2]) do print(item) end
-
     db:close()
+
+    create_todolist_window()
 end
 
 return M
--- lua/todo/init.lua
+++ lua/todo/init.lua
@@ -2,7 +2,6 @@
 if (vim.g.loaded_todo_plugin ~= nil) then
 	return
 end
-
 vim.g.loaded_todo_plugin = 1
 
 -- Define the path where LuaRocks dependencies will be cloned
@@ -10,10 +9,6 @@ local lua_rocks_deps_loc = vim.fn.fnamemodify(debug.getinfo(1, "S").source:sub(2
 -- Extend the Lua package path to include the LuaRocks dependencies
 package.path = package.path .. ";" .. lua_rocks_deps_loc .. "/lua-?/init.lua"
 
--- package.path = './deps/lsqlite3/;' .. package.path
--- package.path = './deps/xsys/;' .. package.path
--- package.path = './deps/lsqlite3/?.lua;' .. package.path
--- package.path = './deps/xsys/?.lua;' .. package.path
 local fetch = require("todo.fetch")
 local update = require("todo.update")
 
@@ -27,7 +22,6 @@ local M = {}
 M.fetch_todos = fetch.fetch_todos
 M.insert_todo = update.insert_todo
 M.complete_todo = update.complete_todo
-
 
 -- Creates the commands that can bell from your remaps
 -- This is the lua equivelant of `command! -nargs=0 FetchTodos lua require("").fetch_todos()`
